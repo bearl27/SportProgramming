3a63e0d802c739a3347307c96d3a87c8ebc9b4bc
bearl <125728897+bearl27@users.noreply.github.com>
2024-04-09 00:41:44 +0900

    212,213

---
+++ 212/b.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#define _USE_MATH_DEFINES
+#include<math.h>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<cstdio>
+#include<utility>
+#include<set>
+#include<list>
+#include<cmath>
+#include<stdio.h>
+#include<string.h>
+#include<iomanip>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+using namespace std;
+#define FOR(i, a, b) for (ll i = (a); i <= (b); i++)
+#define REP(i, n) FOR(i, 0, n - 1)
+#define NREP(i, n) FOR(i, 1, n)
+using ll = long long;
+using pii = pair<int, int>;
+using piii = pair<pii, pii>;
+
+///////////////////////////////////////
+
+int main(void) {
+	char a[5];
+	for (int i = 0; i < 5; i++)a[i] = 0;
+	cin >> a;
+	bool same = true;
+	bool step = true;
+	for (int i = 0; i < 3; i++) {
+		if (a[i] != a[i + 1])same = false;
+		if (((a[i] + 1) % 10) != (a[i + 1] % 10))step = false;
+	}
+	if (same || step)cout << "Weak" << endl;
+	else cout << "Strong" << endl;
+	return 0;
+}
---
+++ 213/a.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#define _USE_MATH_DEFINES
+#include<math.h>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<cstdio>
+#include<utility>
+#include<set>
+#include<list>
+#include<cmath>
+#include<stdio.h>
+#include<string.h>
+#include<iomanip>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+using namespace std;
+#define FOR(i, a, b) for (ll i = (a); i <= (b); i++)
+#define REP(i, n) FOR(i, 0, n - 1)
+#define NREP(i, n) FOR(i, 1, n)
+using ll = long long;
+using pii = pair<int, int>;
+using piii = pair<pii, pii>;
+
+///////////////////////////////////////
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    int c = a^b;//xor は ^
+    cout << c << endl;
+}
