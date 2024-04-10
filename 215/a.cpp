d50da5babf68d31d25eb460897b1254bb3a3eed5
bearl <125728897+bearl27@users.noreply.github.com>
2024-04-10 13:10:36 +0900

    216

---
+++ 215/a.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#define _USE_MATH_DEFINES
+#include<math.h>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<cstdio>
+#include<utility>
+#include<set>
+#include<list>
+#include<cmath>
+#include<stdio.h>
+#include<string.h>
+#include<iomanip>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+
+///////////////////////////////////////
+
+int main(){
+    string str;
+    cin >> str;
+    string ans = "Hello,World!";
+    if(str == ans) cout << "AC" << endl;
+    else cout << "WA" << endl;
+    return 0;
+}
