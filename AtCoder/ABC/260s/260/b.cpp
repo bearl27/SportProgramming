23058a28618c7edf1d1d00b810629e9ef96be7d7
bearl <125728897+bearl27@users.noreply.github.com>
2024-05-03 23:05:28 +0900

    no message

--- AtCoder/ABC/260s/260/b.cpp
+++ AtCoder/ABC/260s/260/b.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 bool compMath(const vector<int>& a, const vector<int>& b){
+    if(a[1] == b[1]){
+        return a[0] < b[0];
+    }
     return a[1] > b[1];
 }
 
---
+++ AtCoder/ABC/260s/262/c.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<cstdio>
+#include<utility>
+#include<set>
+#include<list>
+#include<cmath>
+#include<iomanip>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vpii = vector<pii>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define rep1(i,n) for(int i = 1; i < (n); i++)
+
+///////////////////////////////////////
+
+int main(){
+
+    return 0;
+}
